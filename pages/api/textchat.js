7c36dab1ed67ef4129beea1d3476269247638ef6
derickdecesare <deerriicckk@gmail.com>
2022-12-23 11:53:45 -0800

    added basic twilio api route

---
+++ pages/api/textchat.js
@@ -0,0 +1,18 @@
+
+
+
+export default async function handler (req, res) {
+
+    console.log('api started')
+    const accountSid = process.env.TWILIO_ACCOUNT_SID
+    const authToken = process.env.TWILIO_AUTH_TOKEN
+    const client = require('twilio')(accountSid, authToken)
+
+    client.messages.create({
+        body:'this is my message',
+        from:'+17262009979',
+        to:'+13038286690'
+    })
+    .then(message => console.log(message.sid))
+
+}
