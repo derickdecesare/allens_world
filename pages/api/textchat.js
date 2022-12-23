e61485683a2b357c9ab539c615d14e7460252872
derickdecesare <deerriicckk@gmail.com>
2022-12-23 12:24:37 -0800

    should respond to any phone now

--- pages/api/textchat.js
+++ pages/api/textchat.js
@@ -7,17 +7,24 @@ const { MessagingResponse } = require('twilio').twiml;
 const twiml = new MessagingResponse();
 
 console.log(req.body)
+res.status(200).send('success')
 console.log('editing api')
     console.log('api started')
     const accountSid = process.env.TWILIO_ACCOUNT_SID
     const authToken = process.env.TWILIO_AUTH_TOKEN
     const client = require('twilio')(accountSid, authToken)
 
-    client.messages.create({
-        body:'this is my message',
-        from:'+17262009979',
-        to:'+13038286690'
-    })
-    .then(message => console.log(message.sid))
+    // client.messages.create({
+    //     body:'this is my message',
+    //     from:'+17262009979',
+    //     to:'+13038286690'
+    // })
+    // .then(message => console.log(message.sid))
+
+    twiml.message('Hello, thanks for texting us!');
+      // Set the Content-Type header and send the response
+      res.setHeader('Content-Type', 'text/xml');
+      res.status(200).send(twiml.toString());
+      return;
 
 }
