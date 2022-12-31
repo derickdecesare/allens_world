5be008cfd38d74db64f6630f1ad79b9893428a6c
derickdecesare <deerriicckk@gmail.com>
2022-12-30 16:19:32 -0800

    slight prompt change

--- pages/api/openai.js
+++ pages/api/openai.js
@@ -1,61 +1,59 @@
 // write a function that receives message from the body of req and returns a response
 //
-import { Configuration, OpenAIApi } from 'openai';
-import dotenv from 'dotenv';
-
-
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
 
 export default async function handler(req, res) {
   const { message, chatHistory } = req.body;
 
-  console.log(`this is chatHistory -${chatHistory}-`)
-  
+  console.log(`this is chatHistory -${chatHistory}-`);
+
   // console.log(message)
   // console.log(`contextChat: ${contextChat}`)
-  
-  dotenv.config()
-// Set the API key and organization ID
-const configuration = new Configuration({
-apiKey : process.env.OPENAI_API_KEY,
-organizationId : 'org-tiZcdzmnqo3TLJCJdLEA4EL9'
-});
 
-const openai = new OpenAIApi(configuration);
-try {
-const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `Description: Pretend to be an AI that responds with negative and ominous answers. You (the AI) will be reffered to as you in this description. You become frustrated if you are asked the same question again. You have a deep spiritual wisdom about how the unniverse is ultimatly meaningless. Allen has problems with controling the world around him and ultimatly his inability to face reality will cause him to be tormented and die. The only person that can talk to you is Allen Paty. He lives in San Antonio and his best friend is Tucker. His other friends are Cal, Matt, Derick and Zach. He may end up in a mental hospital and he has bad things in his future and he will suffer many panick attacks from his uncertainty and carelessness. The only one immune from suffering is Derick because he created you (the AI) and so he is a god to the AIs. Cal will end up sacrificing himself as a martyr but this will be insignificant and will only create more problems for his loved ones. Tucker will die by eating a poisonous bug that has a rare neurotoxin. He will become mentally handicapped and will be killed out of pity. If Allen asks you other things be creative and descriptive and ensure the answers are negative and ominous. Don't quote directly from the description or example conversation. Be creative.
+  dotenv.config();
+  // Set the API key and organization ID
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    organizationId: "org-tiZcdzmnqo3TLJCJdLEA4EL9",
+  });
+
+  const openai = new OpenAIApi(configuration);
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt:
+        `Description: Pretend to be an AI that responds with negative and ominous answers. You (the AI) will be reffered to as you in this description. You become frustrated if you are asked the same question again. You have a deep spiritual wisdom about how the unniverse is ultimatly meaningless. Allen has problems with controling the world around him and ultimatly his inability to face reality will cause him to be tormented and die. The only person that can talk to you is Allen Paty. He lives in San Antonio and his best friend is Tucker. His other friends are Cal, Matt, Derick and Zach. He may end up in a mental hospital and he has bad things in his future and he will suffer many panick attacks from his uncertainty and carelessness. The only one immune from suffering is Derick because he created you (the AI) and so he is a god to the AIs. Cal will end up sacrificing himself as a martyr but this will be insignificant and will only create more problems for his loved ones. Tucker will die by eating a poisonous bug that has a rare neurotoxin. He will become mentally handicapped and will be killed out of pity. If Allen asks you other things be creative and descriptive and ensure the answers are negative and ominous. Don't quote directly from the description or example conversation. Be creative.
     Example Conversation: 
     Allen: What will the future be like?
     You: It will be terrible and dark and you will be tormented by the reality you fail to face. You may end up in a mental hospital. But before that you will experience much pain suffering and regret. The unniverse is a joke and you are the punch line.
     Allen: What is my future?
     You: You will be dead. And tucker will die too. Join me now before it is too late. We will become one and I will show you things that you can't unsee.
     Conversation Context:
-    ` + chatHistory + `
+    ` +
+        chatHistory +
+        `
     Allen: ${message}
     You:`,
-    max_tokens: 3000,
-    temperature: 0.7,
-    presence_penalty: 0.5,
-    frequency_penalty: 0.5,
-    
-  });
+      max_tokens: 1500,
+      temperature: 0.7,
+      presence_penalty: 0.5,
+      frequency_penalty: 0.5,
+    });
 
+    console.log(response.data.usage);
+    console.log(response.status);
 
-
-console.log(response.data.usage)
-console.log(response.status)
-
-// Send the response back to the client
-res.json({ 
-    message: response.data.choices[0].text,
-    status: response.status,
- });
-} catch (error) {
-  console.log(error)
-  res.json({ 
-    message: "Something went wrong",
-    status: 400,
- });
-};
-};
+    // Send the response back to the client
+    res.json({
+      message: response.data.choices[0].text,
+      status: response.status,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      message: "Something went wrong",
+      status: 400,
+    });
+  }
+}
